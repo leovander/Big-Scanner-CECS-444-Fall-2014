29c79b91f874dd715323148841c5dba4aad0947a
Israel Torres <itorres1490@gmail.com>
2014-11-20 03:55:42 -0800

    Created Tabbed Panes 	- Reserved word list parsed and displayed 	- State, Action, and Lookup Displayed

--- src/BigScanner.java
+++ src/BigScanner.java
@@ -1,14 +1,24 @@
+import java.awt.Font;
+import java.awt.TextArea;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class BigScanner {
 	int current_read = 0; 
 	int state = 0;
 	String token_under_construction = "";
+	ArrayList<String> sybmolTable = new ArrayList<String>();
+	ArrayList<String> reservedWords = new ArrayList<String>();
 	
 	final int [][] state_table = new int[145][33];
 	final int [][] action_table = new int[145][33];
@@ -16,6 +26,95 @@ public class BigScanner {
 	
 	public BigScanner() throws IOException {
 		xmlToJava.createTables(state_table, action_table, look_up_table);
+	}
+	
+	void displayTables(TextArea state, TextArea action, TextArea look_up) {
+		state.append(String.format("%c%7c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8s%8s",
+				' ', 'L', 'D', '_', '`', '<', '>', '[', ']', '{', '}', '@', '&', '#', '!', '~', '\'', '"', '$', ':', ';', '.', ',', '+',
+				'-', '/', '*', '=', '^', '(', ')', '\\', "EOL", "Other"));
+		state.append("\n");
+		
+		action.append(String.format("%c%7c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8s%8s",
+				' ', 'L', 'D', '_', '`', '<', '>', '[', ']', '{', '}', '@', '&', '#', '!', '~', '\'', '"', '$', ':', ';', '.', ',', '+',
+				'-', '/', '*', '=', '^', '(', ')', '\\', "EOL", "Other"));
+		action.append("\n");
+		
+		look_up.append(String.format("%c%7c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8c%8s%8s",
+				' ', 'L', 'D', '_', '`', '<', '>', '[', ']', '{', '}', '@', '&', '#', '!', '~', '\'', '"', '$', ':', ';', '.', ',', '+',
+				'-', '/', '*', '=', '^', '(', ')', '\\', "EOL", "Other"));
+		look_up.append("\n");
+		
+		for(int i = 0; i < 145; i++) {
+			state.setFont(new Font("Courier New", Font.PLAIN, 12));  
+			state.append(String.format("%s%7d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d",
+										("S" + i), state_table[i][0], state_table[i][1], state_table[i][2], state_table[i][3], state_table[i][4],
+										state_table[i][5], state_table[i][6], state_table[i][7], state_table[i][8], state_table[i][9],
+										state_table[i][10], state_table[i][11], state_table[i][12], state_table[i][13], state_table[i][14],
+										state_table[i][15], state_table[i][16], state_table[i][17], state_table[i][18], state_table[i][19],
+										state_table[i][20], state_table[i][21], state_table[i][22], state_table[i][23], state_table[i][24],
+										state_table[i][25], state_table[i][26], state_table[i][27], state_table[i][28], state_table[i][29],
+										state_table[i][30], state_table[i][31], state_table[i][32]));
+			state.append("\n");
+			
+			action.setFont(new Font("Courier New", Font.PLAIN, 12));  
+			action.append(String.format("%s%7d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d",
+										("S" + i), action_table[i][0], action_table[i][1], action_table[i][2], action_table[i][3], action_table[i][4],
+										action_table[i][5], action_table[i][6], action_table[i][7], action_table[i][8], action_table[i][9],
+										action_table[i][10], action_table[i][11], action_table[i][12], action_table[i][13], action_table[i][14],
+										action_table[i][15], action_table[i][16], action_table[i][17], action_table[i][18], action_table[i][19],
+										action_table[i][20], action_table[i][21], action_table[i][22], action_table[i][23], action_table[i][24],
+										action_table[i][25], action_table[i][26], action_table[i][27], action_table[i][28], action_table[i][29],
+										action_table[i][30], action_table[i][31], action_table[i][32]));
+			action.append("\n");
+			
+			look_up.setFont(new Font("Courier New", Font.PLAIN, 12));  
+			look_up.append(String.format("%s%7d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d%8d",
+										("S" + i), look_up_table[i][0], look_up_table[i][1], look_up_table[i][2], look_up_table[i][3], look_up_table[i][4],
+										look_up_table[i][5], look_up_table[i][6], look_up_table[i][7], look_up_table[i][8], look_up_table[i][9],
+										look_up_table[i][10], look_up_table[i][11], look_up_table[i][12], look_up_table[i][13], look_up_table[i][14],
+										look_up_table[i][15], look_up_table[i][16], look_up_table[i][17], look_up_table[i][18], look_up_table[i][19],
+										look_up_table[i][20], look_up_table[i][21], look_up_table[i][22], look_up_table[i][23], look_up_table[i][24],
+										look_up_table[i][25], look_up_table[i][26], look_up_table[i][27], look_up_table[i][28], look_up_table[i][29],
+										look_up_table[i][30], look_up_table[i][31], look_up_table[i][32]));
+			look_up.append("\n");
+		}
+	}
+	
+	void setReserved(String fileName, TextArea reservedArea) throws IOException {
+		reservedWords.clear();
+		Scanner scanner = new Scanner(new File(fileName));
+		
+		while(scanner.hasNext()) {
+			String line = scanner.nextLine();
+			line.trim();
+			reservedWords.add(line);
+		}
+		scanner.close();
+		
+		Collections.sort(reservedWords);
+		
+		int count = 1;
+		for(String word: reservedWords) {
+			if(count % 5 == 0) {
+				reservedArea.append(word + "\n");
+			} else {
+				reservedArea.append(word + "\t\t");
+			}
+			count++;
+		}
+		
+		reservedArea.setCaretPosition(0);
+	}
+	
+	//Searches through the sorted word list
+	void searchReserved(String query, JLabel label) {
+		if(reservedWords.contains(query)) {
+			label.setText(query + " is a reserved word.");
+		} else if(reservedWords.isEmpty()){
+			label.setText("Enter File Name Above.");
+		} else {
+			label.setText(query + " is NOT a reserved word.");
+		}
 	}
 	
 	void read_characters(String fileName) throws IOException {
@@ -183,64 +282,64 @@ public class BigScanner {
 
 				switch (look_up(state, current_read)) {
 					case 1:
-						System.out.println("TOKEN DISCOVERED IS IDENTIFIER \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS IDENTIFIER \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 2:
-						System.out.println("TOKEN DISCOVERED IS INTEGER \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS INTEGER \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 3:
-						System.out.println("TOKEN DISCOVERED IS SIMPLE OP \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SIMPLE OP \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 4:
-						System.out.println("TOKEN DISCOVERED IS REAL \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS REAL \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 5:
-						System.out.println("TOKEN DISCOVERED IS COMPOUND OP \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS COMPOUND OP \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 6:
-						System.out.println("TOKEN DISCOVERED IS STRING \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS STRING \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 7:
-						System.out.println("TOKEN DISCOVERED IS SINGLE COMMENT \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SINGLE COMMENT \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 8:
-						System.out.println("TOKEN DISCOVERED IS SIGNED INT OR SIGNED INT COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SIGNED INT OR SIGNED INT COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 9:
-						System.out.println("TOKEN DISCOVERED IS BLOCK COMMENT \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS BLOCK COMMENT \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 10:
-						System.out.println("TOKEN DISCOVERED IS CURRENCY \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS CURRENCY \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 11:
-						System.out.println("TOKEN DISCOVERED IS SIGNED REAL \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SIGNED REAL \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 12:
-						System.out.println("TOKEN DISCOVERED IS INT COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS INT COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 13:
-						System.out.println("TOKEN DISCOVERED IS DEVICE \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS DEVICE \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 14:
-						System.out.println("TOKEN DISCOVERED IS SCIENTIFIC \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SCIENTIFIC \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 15:
-						System.out.println("TOKEN DISCOVERED IS REAL COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS REAL COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 16:
-						System.out.println("TOKEN DISCOVERED IS LIBRARY ANGLE \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS LIBRARY ANGLE \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 17:
-						System.out.println("TOKEN DISCOVERED IS LIBRARY QUOTE \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS LIBRARY QUOTE \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 18:
-						System.out.println("TOKEN DISCOVERED IS SIGNED REAL OR SIGNED COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS SIGNED REAL OR SIGNED COMMA \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					case 19:
-						System.out.println("TOKEN DISCOVERED IS LIBRARY \t=> " + token_under_construction.toUpperCase() + "\n");
+						//System.out.println("TOKEN DISCOVERED IS LIBRARY \t=> " + token_under_construction.toUpperCase() + "\n");
 						break;
 					default:
-						System.out.println("error\n");
+						//System.out.println("error\n");
 						break;
 				}
 				
@@ -254,7 +353,7 @@ public class BigScanner {
 			}
 		}
 		
-		System.out.println("DONE SCANNING");
+		//System.out.println("DONE SCANNING");
 	}
 	
 	int look_up(int new_state, int new_char) {
--- src/ScannerWindow.java
+++ src/ScannerWindow.java
@@ -1,13 +1,101 @@
 import java.io.IOException;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JScrollPane;
+import java.awt.TextArea;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+
 
 public class ScannerWindow {
+	private JFrame frmBigScanner;
+
 	public static void main(String[] args) throws IOException {
 		BigScanner scanner = new BigScanner();
-		scanner.read_characters("source.txt");
+		ScannerWindow window = new ScannerWindow(scanner);
+		window.frmBigScanner.setVisible(true);
+		
+		//scanner.read_characters("source.txt");
 	}
 	
-	public ScannerWindow() {
+	/**
+	 * @throws IOException 
+	 * @wbp.parser.entryPoint
+	 */
+	public ScannerWindow(BigScanner scanner) throws IOException {
+		frmBigScanner = new JFrame();
+		frmBigScanner.setTitle("Big Scanner");
+		frmBigScanner.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmBigScanner.setResizable(false);
+		frmBigScanner.setBounds(0, 0, 1000, 600);
 		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frmBigScanner.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Main", null, panel, "Enter source file to scan");
+		panel.setLayout(null);
+		tabbedPane.setEnabledAt(0, true);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Reserved Words", null, panel_1, "List of words supported");
+		tabbedPane.setEnabledAt(1, true);
+		panel_1.setLayout(null);
+		
+		TextArea textArea = new TextArea();
+		textArea.setBounds(10, 10, 959, 337);
+		panel_1.add(textArea);
+		textArea.setEditable(false);
+		
+		//TODO: Fill reversed ArrayList<String>
+		scanner.setReserved("reservedWord.txt", textArea);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Tables", null, panel_2, "Display of State, Action, and Lookup Tables");
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		panel_2.add(tabbedPane_1);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane_1.addTab("State", null, panel_3, "State Table from XML");
+		tabbedPane_1.setEnabledAt(0, true);
+		panel_3.setLayout(null);
+		
+		TextArea textArea_1 = new TextArea();
+		textArea_1.setEditable(false);
+		textArea_1.setBounds(10, 10, 938, 466);
+		panel_3.add(textArea_1);
+		tabbedPane_1.setBackgroundAt(0, Color.BLUE);
+		
+		JPanel panel_4 = new JPanel();
+		tabbedPane_1.addTab("Action", null, panel_4, "Action Table generated from XML");
+		panel_4.setLayout(null);
+		
+		TextArea textArea_2 = new TextArea();
+		textArea_2.setEditable(false);
+		textArea_2.setBounds(10, 10, 938, 466);
+		panel_4.add(textArea_2);
+		tabbedPane_1.setBackgroundAt(1, Color.GREEN);
+		
+		JPanel panel_5 = new JPanel();
+		tabbedPane_1.addTab("Lookup", null, panel_5, "Lookup Table generated from XML");
+		panel_5.setLayout(null);
+		
+		TextArea textArea_3 = new TextArea();
+		textArea_3.setEditable(false);
+		textArea_3.setBounds(10, 10, 938, 466);
+		panel_5.add(textArea_3);
+		tabbedPane_1.setBackgroundAt(2, Color.PINK);
+		
+		//TODO: Fill State, Action, and Lookup Tables
+		scanner.displayTables(textArea_1, textArea_2, textArea_3);
 	}
 }
